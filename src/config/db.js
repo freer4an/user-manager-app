5b1ebc9b45a6be74148c3f486d8e543201d1cd7e
freer4an <freer4an@gmail.com>
2024-02-27 01:11:50 +0600

    bad config

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-const dbHost = process.env.DB_HOST || "localhost";
-const dbPort = process.env.DB_PORT || 5432;
-const dbUser = process.env.DB_USER || "postgres";
-const dbName = process.env.DB_NAME || "user-management";
-const dbPass = process.env.DB_PASSWORD || "pass";
+// const dbHost = process.env.DB_HOST || "localhost";
+// const dbPort = process.env.DB_PORT || 5432;
+// const dbUser = process.env.DB_USER || "postgres";
+// const dbName = process.env.DB_NAME || "user-management";
+// const dbPass = process.env.DB_PASSWORD || "pass";
+//
+// const db = new Sequelize(dbName, dbUser, dbPass, {
+//     host: dbHost,
+//     port: dbPort,
+//     dialect: "postgres"
+// })
 
-const db = new Sequelize(dbName, dbUser, dbPass, {
-    host: dbHost,
-    port: dbPort,
-    dialect: "postgres"
-})
+const dbUri = process.env.DATABASE_URL || "postgres://postgres:pass@localhost:5432/user-management"
+
+const db = new Sequelize(dbUri)
 
 module.exports = db
