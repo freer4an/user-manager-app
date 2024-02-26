58da1fcd8a8a5a505f8a0084a73226fe5fb8350c
freer4an <freer4an@gmail.com>
2024-02-27 00:55:59 +0600

    bad config

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,11 @@ const { Sequelize } = require('sequelize');
 
 const dbHost = process.env.DB_HOST || "localhost";
 const dbPort = process.env.DB_PORT || 5432;
+const dbUser = process.env.DB_USER || "postgres";
+const dbName = process.env.DB_NAME || "user-management";
+const dbPass = process.env.DB_PASSWORD || "pass";
 
-const db = new Sequelize("user-management", "postgres", "pass", {
+const db = new Sequelize(dbName, dbUser, dbPass, {
     host: dbHost,
     port: dbPort,
     dialect: "postgres"
